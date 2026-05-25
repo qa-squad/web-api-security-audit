a4e6b4f4550ba3f3d2419a23cc1f6f692a8f6971
iffatjahan <jahan88iffat@gmail.com>
2026-05-25 22:54:27 +1000

    Add first working basic audit script

---
+++ src/scanner/basicAudit.ts
@@ -0,0 +1,45 @@
+import { chromium } from 'playwright';
+import fs from "fs";
+
+async function runAudit() {
+    const browser = await chromium.launch({ headless: true });
+    const page = await browser.newPage();
+
+    const url = 'https://example.com';
+
+    const consoleErrors: string[] = [];
+
+    page.on('console', msg => {
+        if (msg.type() === 'error') {
+            consoleErrors.push(msg.text());
+        }
+    });
+
+    const response = await page.goto(url);
+
+    const title = await page.title();
+
+    await page.screenshot({
+        path: 'screenshots/homepage.png',
+        fullPage: true
+    });
+
+    const report = {
+        url,
+        title,
+        status: response?.status(),
+        consoleErrors
+    };
+
+    fs.writeFileSync(
+        'sample-reports/basic-report.json',
+        JSON.stringify(report, null, 2)
+    );
+
+    console.log('Audit Complete');
+    console.log(report);
+
+    await browser.close();
+}
+
+runAudit();
